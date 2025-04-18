5432f80d80858ed1eb5a045e66dc2be49ea2a25b
i-am-surovi <ssur.ovi03@gmail.com>
2025-04-19 01:14:38 +0600

    educator identifier added

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { AppContext } from "../../context/AppContext";
 
 const Navbar = () => {
+
+  const {navigate, isEducator} = useContext(AppContext)
+
   const isCourseListPage = location.pathname.includes("/course-list");
 
   const { openSignIn } = useClerk();
@@ -16,6 +20,7 @@ const Navbar = () => {
       }`}
     >
       <img
+        onClick={()=>navigate('/')}
         src={assets.logo}
         alt="Logo"
         className="w-28 lg:w-32 cursor-pointer"
@@ -26,11 +31,11 @@ const Navbar = () => {
         <div className="flex items-center gap-5">
           {user && (
             <>
-              <button>Become Educator</button>|{" "}
+              <button onClick={()=> {navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>|
               <Link to="/my-enrollments">My Enrollments</Link>
             </>
           )}
-        </div>
+        </div> 
         {user ? (
           <UserButton />
         ) : (
@@ -48,8 +53,8 @@ const Navbar = () => {
         <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
           {user && (
             <>
-              <button>Become Educator</button>|{" "}
-              <Link to="/my-enrollments">My Enrollments</Link>
+              <button onClick={()=> {navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
+              |<Link to="/my-enrollments">My Enrollments</Link>
             </>
           )}
         </div>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,17 +1,32 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props)=>{
 
     const currency = import.meta.env.VITE_CURRENCY
+    const navigate = useNavigate()
 
     const [allCourses, setAllCourses] = useState([])
+    const [isEducator, setIsEducator] = useState([true])
 
     // Fetch All Courses
     const fetchAllCourses = async ()=>{
         setAllCourses(dummyCourses)
+    }
+
+    // Functions to calculate average rating of course
+    const calculateRating = (course)=>{
+        if(course.courseRatings.length === 0){
+            return 0;
+        }
+        let totalRating = 0
+        course.courseRatings.forEach(rating => {
+            totalRating += rating.rating
+        })
+        return totalRating / course.courseRatings.length 
     }
 
     useEffect(()=>{
@@ -19,7 +34,7 @@ export const AppContextProvider = (props)=>{
     },[])
 
     const value = {
-        currency, allCourses
+        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator
     }
 
     return (
