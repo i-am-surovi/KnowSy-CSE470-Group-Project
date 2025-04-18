5dff537b195cc21db161b396ee1422215c9dbaad
i-am-surovi <ssur.ovi03@gmail.com>
2025-04-19 02:32:55 +0600

    Footer added

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -3,6 +3,8 @@ import Hero from "../../components/student/Hero";
 import Companies from "../../components/student/Companies";
 import CourseSection from "../../components/student/CourseSection";
 import TestimonialSection from "../../components/student/TestimonialSection";
+import CallToAction from "../../components/student/CallToAction";
+import Footer from "../../components/student/Footer";
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
       <Companies/>
       <CourseSection/>
       <TestimonialSection/>
+      <CallToAction/>
+      <Footer/>
     </div>
   );
 };
