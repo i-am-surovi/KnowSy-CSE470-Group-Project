5ed4f6e9416d035aa363a07fe1a74fb5788019be
i-am-surovi <ssur.ovi03@gmail.com>
2025-04-19 00:00:33 +0600

    course section  added

--- client/src/components/student/CourseSection.jsx
+++ client/src/components/student/CourseSection.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const CourseSection = () => {
   return (
-    <div>
-      <h1>CourseSection</h1>
-    </div>
-  )
-}
+    <div className="py-16 md:px-40 px-8">
+      <h2 className="text-center text-3xl font-medium text-gray-800">
+        Learn from the best
+      </h2>
+      <p className="text-sm md:text-center text-gray-500 mt-3">
+        Discover our top-rated courses across various categories. From coding
+        and design to business and wellness, our courses are crafted to deliver
+        results.
+      </p>
 
-export default CourseSection
+      <div className='mt-6 flex justify-center'>
+        <Link
+          to={"/course-list"}
+          onClick={() => scrollTo(0, 0)}
+          className="text-gray-500 border border-gray-500/30 px-10 py-3 rounded"
+        >
+          Show all courses
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CourseSection;
