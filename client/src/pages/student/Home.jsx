0c26d6a7782600a27b10c513e1e75d7f52580bb6
i-am-surovi <ssur.ovi03@gmail.com>
2025-04-19 01:47:00 +0600

    testimonial section added

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Hero from "../../components/student/Hero";
 import Companies from "../../components/student/Companies";
 import CourseSection from "../../components/student/CourseSection";
+import TestimonialSection from "../../components/student/TestimonialSection";
 
 const Home = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center space-y-7 text-center">
       <Hero/>
       <Companies/>
       <CourseSection/>
+      <TestimonialSection/>
     </div>
   );
 };
