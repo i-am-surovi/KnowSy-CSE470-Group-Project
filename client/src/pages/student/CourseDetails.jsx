d167fbfda7bfc8a89f6dfcbf21250bf95204afc2
deeanatrahman <deeanat.rahman@g.bracu.ac.bd>
2025-04-19 13:48:02 +0600

    Added Course-list page

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useContext } from 'react'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
 
 const CourseDetails = () => {
+
+  const {id} = useParams()
+
+  const [courseData, setCourseData] = useState(null)
+
+  const {allCourses} = useContext(AppContext)
+
+  const fetchCourseData = async ()=>{
+    const findCourse = allCourses.find(course => course._id === id)
+    setCourseData(findCourse);
+  }
+
+  useEffect(()=>{
+    fetchCourseData()
+  }, [])
+
   return (
-    <div>
-      <h1>Course Details page</h1>
+    <div >
+      {/* left column */}
+      <div></div>
+      {/* right column */}
+      <div></div>
     </div>
   )
 }
