8f739487fd23b6a71ec2377a872f69fede088f26
i-am-surovi <ssur.ovi03@gmail.com>
2025-04-20 00:36:51 +0600

    course-details added before enrollemnt section

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -11,14 +11,14 @@ const CourseCard = ({ course }) => {
       <img className="w-full" src={course.courseThumbnail} alt="" />
       <div className="p-3 text-left">
         <h3 className="text-base font-semibold">{course.courseTitle}</h3>
-        <p className="text-gray-500">{course.educator.name}</p>
+        <p className="text-gray-500">Knowsy</p>
         <div className="flex items-center space-x-2">
           <p>{calculateRating(course)}</p>
           <div className="flex">
             {[...Array(5)].map((_, i) => (
               <img key={i} src={i < Math.floor(calculateRating(course)) ? assets.star : assets.star_blank} alt="" 
               className="w-3.5 h-3.5"/>
-            ))}
+            ))} 
           </div>
           <p className="text-gray-500">{course.courseRatings.length}</p>
         </div>
