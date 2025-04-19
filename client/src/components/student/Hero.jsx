5b776ff50226f8cfbae7483144e97556e4f500e9
i-am-surovi <ssur.ovi03@gmail.com>
2025-04-19 13:04:48 +0600

    Another touch-up

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -4,7 +4,6 @@ import SearchBar from "./SearchBar";
 
 const Hero = () => {
   return (
-    <div>
       <div className="flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70">
         <h1 className="md:text-home-heading-large text-home-heading-small relative font-bold text-gray-800 max-w-3xl mx-auto">
           Empower your future with the courses designed to{" "}
@@ -29,7 +28,6 @@ const Hero = () => {
 
         <SearchBar/>
       </div>
-    </div>
   );
 };
 
