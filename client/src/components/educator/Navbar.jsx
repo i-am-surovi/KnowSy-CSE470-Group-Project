69ad3c52e6894fabf73d3b5f76e431fe2d604c71
deeanatrahman <deeanat.rahman@g.bracu.ac.bd>
2025-04-21 03:14:24 +0600

    watching lectures completed

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { dummyEducatorData } from '../../assets/assets';
+import {UserButton, useUser} from '@clerk/clerk-react'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const educatorData = dummyEducatorData
+  const { user } = useUser()
   return (
     <div>
-      <h1>Edu Navbar</h1>
+      <Link to='/'>
+      <img src={assets.logo} alt="Logo" className='w-28 lg:w-32'/>
+      </Link>
+      <div>
+      <p>Hi! {User ? user.fullName : 'Developers'}</p>
+      {user ? <UserButton /> : <img className='max-w-8' src= {assets.profile_img} />}
+      </div>
     </div>
   )
 }
