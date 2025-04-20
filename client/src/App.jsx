af11a8e360e2940ac21e823552a2aa8ea2545857
i-am-surovi <ssur.ovi03@gmail.com>
2025-04-20 20:32:53 +0600

    course progress-bar added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
         <Route path='/course-list/:input' element={<CoursesList />}/>
         <Route path='/course/:id' element={<CourseDetails/>}/>
         <Route path='/my-enrollments' element={<MyEnrollments/>}/>
-        <Route path='/player/courseId' element={<Player/>}/>
+        <Route path='/player/:courseId' element={<Player/>}/>
         <Route path='/loading/:path' element={<Loading/>}/>
         <Route path='/educator' element={<Educator/>}>
             <Route path='dashboard' element={<Dashboard/>}/>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
   const currency = import.meta.env.VITE_CURRENCY;
-  const navigate = useNavigate();
+  const navigate = useNavigate(); 
 
   const [allCourses, setAllCourses] = useState([]);
   const [isEducator, setIsEducator] = useState(true);
