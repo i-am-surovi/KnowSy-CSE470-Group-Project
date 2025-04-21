f72729938b0719ecba1c4b74236983e27c6c47dc
deeanatrahman <deeanat.rahman@g.bracu.ac.bd>
2025-04-21 19:03:46 +0600

    educators sidebar added

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { dummyEducatorData } from '../../assets/assets';
+import { assets, dummyEducatorData } from '../../assets/assets';
 import {UserButton, useUser} from '@clerk/clerk-react'
 import { Link } from 'react-router-dom';
 
@@ -7,12 +7,12 @@ const Navbar = () => {
   const educatorData = dummyEducatorData
   const { user } = useUser()
   return (
-    <div>
+    <div className='flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3'>
       <Link to='/'>
       <img src={assets.logo} alt="Logo" className='w-28 lg:w-32'/>
       </Link>
       <div className='flex items-center gap-5 text-gray-500 relative'>
-      <p>Hi! {User ? user.fullName : 'Developers'}</p>
+      <p>Hi! {user ? user.fullName : 'Developers'}</p>
       {user ? <UserButton /> : <img className='max-w-8' src= {assets.profile_img} />}
       </div>
     </div>
