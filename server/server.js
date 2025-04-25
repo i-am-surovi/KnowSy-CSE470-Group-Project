c72fae6c87f257b99e11498cac434633647b3133
fairuz04 <fairuz.suhala.reza@g.bracu.ac.bd>
2025-04-25 21:36:31 +0600

    user model added

--- server/server.js
+++ server/server.js
@@ -2,22 +2,32 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
+import { clerkWebhooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educatorRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
+import connectCloudinary from './configs/cloudinary.js'
 
-// Initialized Express
+// Initialize Express
 const app = express()
 
 // Connect to database
 await connectDB()
 
+// Connect to Cloudinary
+await connectCloudinary()
+
 // Middlewares
 app.use(cors())
+app.use(clerkMiddleware())
 
-// Routes
-app.get('/', (req, res)=> res.send("API Working"))
+// Route
+app.get('/', (req, res)=> res.send("Server Running Successfully"))
+app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator', express.json(), educatorRouter)
 
-// Port
+//Port
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, ()=>{
+app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`)
 })
