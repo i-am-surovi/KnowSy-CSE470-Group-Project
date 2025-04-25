5c17c7158851db5847cc189fce900df251c4f645
fairuz04 <fairuz.suhala.reza@g.bracu.ac.bd>
2025-04-26 01:42:09 +0600

    fixed educator routes errors and others

--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from "express"
 import { updateRoleToEducator } from "../controllers/educatorController.js";
 
-const educatorRouter = express.Router();
+const educatorRouter = express.Router()
 
 // Add Educator Router
-educatorRouter.get("/updated-role", updateRoleToEducator);
+educatorRouter.get("/update-role", updateRoleToEducator)
 
 export default educatorRouter;
