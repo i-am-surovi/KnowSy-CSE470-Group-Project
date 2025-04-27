3af95ae40523f27ff812c832f2259249c29df8f0
deeanatrahman <deeanat.rahman@g.bracu.ac.bd>
2025-04-28 03:03:11 +0600

    educator dashboard data fetched

--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -9,6 +9,7 @@ const educatorRouter = express.Router()
 educatorRouter.get("/update-role", updateRoleToEducator)
 educatorRouter.post('/add-course', upload.single('image'), protectEducator, addCourse)
 educatorRouter.get('/courses', protectEducator, getEducatorCourses)
+
 educatorRouter.get('/dashboard', protectEducator, educatorDashboardData)
 educatorRouter.get('/enrolled-students', protectEducator, getEnrolledStudentsData)
 
