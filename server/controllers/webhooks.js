4ecbfbab4ebc5389d88cbec6a7f2446720e97725
chimmysaan <196896268+chimmysaan@users.noreply.github.com>
2025-05-01 02:56:39 +0600

    payment intent success

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,5 +1,7 @@
 import { Webhook } from "svix";
 import User from "../models/User.js";
+import Stripe from "stripe";
+
 
 // Api Controller Function to Manage Clerk User with database
 
@@ -51,4 +53,62 @@ export const clerkWebhooks = async (req, res)=>{
     } catch (error) {
         res.json({success: false, message: error.message})
     }
+}
+
+
+const stripeInstance= new Stripe(process.env.STRIPE_SECRET_KEY)
+export const stripeWebhooks= async()=>{
+    const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = Stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+   }
+  catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+  }
+ // Handle the event
+ switch (event.type) {
+    case 'payment_intent.succeeded':{
+      const paymentIntent = event.data.object;
+      const paymentIntentId=paymentIntent.id
+
+      const session= await stripeInstance.checkoout.sessons.list({
+        payment_intent: paymentIntentId
+      })
+
+      const {purchaseId}= session.data[0].metadata;
+
+      const purchaseData= await Purchase.findById(purchaseId)
+      const userData= await User.findById(purchaseData.userId)
+      const courseData= await Course.findById(purchaseData.courseId.tostring())
+
+      courseData.enrolledStudents.push(userData)
+      await courseData.save()
+
+      userData.enrolledCourses.push(courseData._id)
+      await userData.save()
+
+      purchaseData.status.save()
+
+      break;
+    }
+
+    
+
+
+
+    case 'payment_method.attached':
+      const paymentMethod = event.data.object;
+      console.log('PaymentMethod was attached to a Customer!');
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a response to acknowledge receipt of the event
+  response.json({received: true});
+
 }
