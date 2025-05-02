a32a71713707d401953b8cd5d75cfab7211d6882
chimmysaan <196896268+chimmysaan@users.noreply.github.com>
2025-05-02 13:40:38 +0600

    Course Progress Route Adeed

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,4 +5,8 @@ userRouter.get('/data', getUserData)
 userRouter.get('/enrolled-courses', userEnrolledCourses)
 userRouter.post('/purchase', purchaseCourse)
 
+
+userRouter.post('/update-course-progress', updateUserCourseProgress)
+userRouter.get('/get-course-progress', getUserCourseProgress)
+
 export default userRouter;
